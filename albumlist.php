37c66d4cc98c0010ccada0bc4663bed880db2741
Lin Xin <widayn@prmlk.com>
2022-08-14 13:47:51 +0800

    Create albumlist.php

---
+++ albumlist.php
@@ -0,0 +1,124 @@
+<script>
+    let doubanId = "213912517";
+</script>
+
+
+<?php /*Template Name: AlbumList*/ ?>
+<!DOCTYPE html>  
+<?php 
+get_header();
+?>
+
+<style>
+html{font-family:sans-serif;-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%;}body{margin:0;}article,aside,details,figcaption,figure,footer,header,hgroup,main,menu,nav,section,summary{display:block;}audio,canvas,progress,video{display:inline-block;vertical-align:baseline;}audio:not([controls]){display:none;height:0;}[hidden],template{display:none;}a{background-color:transparent;}a:active,a:hover{outline:0;}abbr[title]{border-bottom:1px dotted;}b,strong{font-weight:bold;}dfn{font-style:italic;}h1{font-size:2em;margin:0.67em 0;}mark{background:#ff0;color:#000;}small{font-size:80%;}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline;}sup{top:-0.5em;}sub{bottom:-0.25em;}img{border:0;}svg:not(:root){overflow:hidden;}figure{margin:1em 40px;}hr{-moz-box-sizing:content-box;box-sizing:content-box;height:0;}pre{overflow:auto;}code,kbd,pre,samp{font-family:monospace,monospace;font-size:1em;}button,input,optgroup,select,textarea{color:inherit;font:inherit;margin:0;}button{overflow:visible;}button,select{text-transform:none;}button,html input[type="button"],input[type="reset"],input[type="submit"]{-webkit-appearance:button;cursor:pointer;}button[disabled],html input[disabled]{cursor:default;}button::-moz-focus-inner,input::-moz-focus-inner{border:0;padding:0;}input{line-height:normal;}input[type="checkbox"],input[type="radio"]{box-sizing:border-box;padding:0;}input[type="number"]::-webkit-inner-spin-button,input[type="number"]::-webkit-outer-spin-button{height:auto;}input[type="search"]{-webkit-appearance:textfield;-moz-box-sizing:content-box;-webkit-box-sizing:content-box;box-sizing:content-box;}input[type="search"]::-webkit-search-cancel-button,input[type="search"]::-webkit-search-decoration{-webkit-appearance:none;}fieldset{border:1px solid #c0c0c0;margin:0 2px;padding:0.35em 0.625em 0.75em;}legend{border:0;padding:0;}textarea{overflow:auto;}optgroup{font-weight:bold;}table{border-collapse:collapse;border-spacing:0;}td,th{padding:0;}.row{margin-bottom:20px;}.wp-albumlist-item{width:15em;height:21em;}.wp-albumlist-link{display:block;width:15em;height:21em;text-decoration-line:none;}.wp-albumlist-img{position:absolute;width:15em;height:21em;background-size:100%;box-shadow:0 0 10px rgb(0 0 0 / 10%),0 5px 20px rgb(0 0 0 / 20%);border-radius:1.5em;z-index:-1;filter:blur(1px);transition:filter 0.5s;}.wp-albumlist-description{position:relative;width:15em;height:7em;top:14em;background-color:black;border-radius:0 0 1.5em 1.5em;opacity:0.5;z-index:2;transition:opacity 0.5s;}.wp-albumlist-description > .wp-albumlist-title{font-size:1rem;padding:1em 1em 0.5em 1em;color:white;text-align:center;}.wp-albumlist-description > .wp-albumlist-time{font-size:0.8rem;padding:1em 1em 0.5em 1em;color:white;text-align:center;}.wp-albumlist-item:hover .wp-albumlist-img{filter:blur(0px);}.wp-albumlist-item:hover .wp-albumlist-description{opacity:0;}.wp-albumlist-invisable{opacity:0;}.container{position:relative;width:100%;max-width:960px;margin:0 auto;padding:0 20px;box-sizing:border-box;}.column,.columns{width:100%;float:left;box-sizing:border-box;}@media (min-width:400px){.container{width:85%;padding:0;}}@media (min-width:550px){.container{width:80%;}.column,.columns{margin-left:4%;}.column:first-child,.columns:first-child{margin-left:0;}.one.column,.one.columns{width:4.66666666667%;}.two.columns{width:13.3333333333%;}.three.columns{width:22%;}.four.columns{width:30.6666666667%;}.five.columns{width:39.3333333333%;}.six.columns{width:48%;}.seven.columns{width:56.6666666667%;}.eight.columns{width:65.3333333333%;}.nine.columns{width:74.0%;}.ten.columns{width:82.6666666667%;}.eleven.columns{width:91.3333333333%;}.twelve.columns{width:100%;margin-left:0;}.one-third.column{width:30.6666666667%;}.two-thirds.column{width:65.3333333333%;}.one-half.column{width:48%;}.offset-by-one.column,.offset-by-one.columns{margin-left:8.66666666667%;}.offset-by-two.column,.offset-by-two.columns{margin-left:17.3333333333%;}.offset-by-three.column,.offset-by-three.columns{margin-left:26%;}.offset-by-four.column,.offset-by-four.columns{margin-left:34.6666666667%;}.offset-by-five.column,.offset-by-five.columns{margin-left:43.3333333333%;}.offset-by-six.column,.offset-by-six.columns{margin-left:52%;}.offset-by-seven.column,.offset-by-seven.columns{margin-left:60.6666666667%;}.offset-by-eight.column,.offset-by-eight.columns{margin-left:69.3333333333%;}.offset-by-nine.column,.offset-by-nine.columns{margin-left:78.0%;}.offset-by-ten.column,.offset-by-ten.columns{margin-left:86.6666666667%;}.offset-by-eleven.column,.offset-by-eleven.columns{margin-left:95.3333333333%;}.offset-by-one-third.column,.offset-by-one-third.columns{margin-left:34.6666666667%;}.offset-by-two-thirds.column,.offset-by-two-thirds.columns{margin-left:69.3333333333%;}.offset-by-one-half.column,.offset-by-one-half.columns{margin-left:52%;}}html{font-size:62.5%;}body{font-size:1.5em;line-height:1.6;font-weight:400;font-family:"Raleway","HelveticaNeue","Helvetica Neue",Helvetica,Arial,sans-serif;color:#222;}h1,h2,h3,h4,h5,h6{margin-top:0;margin-bottom:2rem;font-weight:300;}h1{font-size:4.0rem;line-height:1.2;letter-spacing:-.1rem;}h2{font-size:3.6rem;line-height:1.25;letter-spacing:-.1rem;}h3{font-size:3.0rem;line-height:1.3;letter-spacing:-.1rem;}h4{font-size:2.4rem;line-height:1.35;letter-spacing:-.08rem;}h5{font-size:1.8rem;line-height:1.5;letter-spacing:-.05rem;}h6{font-size:1.5rem;line-height:1.6;letter-spacing:0;}@media (min-width:550px){h1{font-size:5.0rem;}h2{font-size:4.2rem;}h3{font-size:3.6rem;}h4{font-size:3.0rem;}h5{font-size:2.4rem;}h6{font-size:1.5rem;}}p{margin-top:0;}a{color:#1EAEDB;}a:hover{color:#0FA0CE;}.button,button,input[type="submit"],input[type="reset"],input[type="button"]{display:inline-block;height:38px;padding:0 30px;color:#555;text-align:center;font-size:11px;font-weight:600;line-height:38px;letter-spacing:.1rem;text-transform:uppercase;text-decoration:none;white-space:nowrap;background-color:transparent;border-radius:4px;border:1px solid #bbb;cursor:pointer;box-sizing:border-box;}.button:hover,button:hover,input[type="submit"]:hover,input[type="reset"]:hover,input[type="button"]:hover,.button:focus,button:focus,input[type="submit"]:focus,input[type="reset"]:focus,input[type="button"]:focus{color:#333;border-color:#888;outline:0;}.button.button-primary,button.button-primary,input[type="submit"].button-primary,input[type="reset"].button-primary,input[type="button"].button-primary{color:#FFF;background-color:#33C3F0;border-color:#33C3F0;}.button.button-primary:hover,button.button-primary:hover,input[type="submit"].button-primary:hover,input[type="reset"].button-primary:hover,input[type="button"].button-primary:hover,.button.button-primary:focus,button.button-primary:focus,input[type="submit"].button-primary:focus,input[type="reset"].button-primary:focus,input[type="button"].button-primary:focus{color:#FFF;background-color:#1EAEDB;border-color:#1EAEDB;}input[type="email"],input[type="number"],input[type="search"],input[type="text"],input[type="tel"],input[type="url"],input[type="password"],textarea,select{height:38px;padding:6px 10px;background-color:#fff;border:1px solid #D1D1D1;border-radius:4px;box-shadow:none;box-sizing:border-box;}input[type="email"],input[type="number"],input[type="search"],input[type="text"],input[type="tel"],input[type="url"],input[type="password"],textarea{-webkit-appearance:none;-moz-appearance:none;appearance:none;}textarea{min-height:65px;padding-top:6px;padding-bottom:6px;}input[type="email"]:focus,input[type="number"]:focus,input[type="search"]:focus,input[type="text"]:focus,input[type="tel"]:focus,input[type="url"]:focus,input[type="password"]:focus,textarea:focus,select:focus{border:1px solid #33C3F0;outline:0;}label,legend{display:block;margin-bottom:.5rem;font-weight:600;}fieldset{padding:0;border-width:0;}input[type="checkbox"],input[type="radio"]{display:inline;}label > .label-body{display:inline-block;margin-left:.5rem;font-weight:normal;}ul{list-style:circle inside;}ol{list-style:decimal inside;}ol,ul{padding-left:0;margin-top:0;}ul ul,ul ol,ol ol,ol ul{margin:1.5rem 0 1.5rem 3rem;font-size:90%;}li{margin-bottom:1rem;}code{padding:.2rem .5rem;margin:0 .2rem;font-size:90%;white-space:nowrap;background:#F1F1F1;border:1px solid #E1E1E1;border-radius:4px;}pre > code{display:block;padding:1rem 1.5rem;white-space:pre;}th,td{padding:12px 15px;text-align:left;border-bottom:1px solid #E1E1E1;}th:first-child,td:first-child{padding-left:0;}th:last-child,td:last-child{padding-right:0;}button,.button{margin-bottom:1rem;}input,textarea,select,fieldset{margin-bottom:1.5rem;}pre,blockquote,dl,figure,table,p,ul,ol,form{margin-bottom:2.5rem;}.u-full-width{width:100%;box-sizing:border-box;}.u-max-full-width{max-width:100%;box-sizing:border-box;}.u-pull-right{float:right;}.u-pull-left{float:left;}hr{margin-top:3rem;margin-bottom:3.5rem;border-width:0;border-top:1px solid #E1E1E1;}.container:after,.row:after,.u-cf{content:"";display:table;clear:both;}@media (min-width:400px){}@media (min-width:550px){}@media (min-width:750px){}@media (min-width:1000px){}@media (min-width:1200px){}
+</style>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="referrer" content="no-referrer">
+</head>
+<body>
+<script>
+    var startIndex = 0;
+
+    function getDocumentTop() {
+        var scrollTop = 0,
+            bodyScrollTop = 0,
+            documentScrollTop = 0;
+        if (document.body) {
+            bodyScrollTop = document.body.scrollTop;
+        }
+        if (document.documentElement) {
+            documentScrollTop = document.documentElement.scrollTop;
+        }
+        scrollTop = (bodyScrollTop - documentScrollTop > 0) ? bodyScrollTop : documentScrollTop;
+        return scrollTop;
+    }
+
+    function getWindowHeight() {
+        var windowHeight = 0;
+        if (document.compatMode === "CSS1Compat") {
+            windowHeight = document.documentElement.clientHeight;
+        } else {
+            windowHeight = document.body.clientHeight;
+        }
+        return windowHeight;
+    }
+
+    function getScrollHeight() {
+        var scrollHeight = 0,
+            bodyScrollHeight = 0,
+            documentScrollHeight = 0;
+        if (document.body) {
+            bodyScrollHeight = document.body.scrollHeight;
+        }
+        if (document.documentElement) {
+            documentScrollHeight = document.documentElement.scrollHeight;
+        }
+        scrollHeight = (bodyScrollHeight - documentScrollHeight > 0) ? bodyScrollHeight : documentScrollHeight;
+        return scrollHeight;
+    }
+
+    function loadPage() {
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.open('GET', 'https://douban.prmlk.com/getList?doubanId='+ doubanId +'&startIndex=' + startIndex, true);
+        httpRequest.send();
+        /**
+         * 获取数据后的处理程序
+         */
+        httpRequest.onreadystatechange = function () {
+            let sb;
+            if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+                var json = httpRequest.responseText;//获取到json字符串，还需解析
+                var obj = JSON.parse(json);
+                var count = 1;
+                let row;
+
+                console.log(obj.data.list)
+
+                for (let item of obj.data.list) {
+                    if (count % 4 === 1) {
+                        row = document.createElement("div");
+                        row.setAttribute("class", "row");
+                    }
+                    sb = '<div class="three columns"><a class="wp-albumlist-link" target="_blank" href="https://movie.douban.com/subject/' + item.SubjectId + '/"> <div class="wp-albumlist-item"> <img class="wp-albumlist-img" src="' + item.SubjectPhoto + '"> <div class="wp-albumlist-description"> <p class="wp-albumlist-title"> ' + item.SubjectName + ' </p> <p class="wp-albumlist-time"> ' + item.ReleaseDate + ' </p></div></div></a></div>';
+                    row.innerHTML += sb;//添加到div里
+                    if (count % 4 === 0) {
+                        document.getElementById("container").appendChild(row)
+                    }
+                    count++;
+                }
+                if (count % 4 !== 1) {
+                    document.getElementById("container").appendChild(row)
+                }
+            }
+        };
+    }
+
+    loadPage();
+
+    window.onscroll = function () {
+        if (getScrollHeight() <= getWindowHeight() + getDocumentTop()) {
+            console.log("到底啦");
+            startIndex += 16;
+            loadPage();
+        }
+
+    }
+
+</script>
+
+
+<!-- Primary Page Layout
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<div style="text-align:center;"><h2>追番列表</h2></div>
+<div class="container" id="container">
+</div>
+
+<!-- End Document
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+</body>
+<?php 
+    get_footer();
+?>
+</html>
